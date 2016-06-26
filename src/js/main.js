c4356be4fce8ac0ae700d4e0758ec31a278427fd
Yury Zaytsev <more.jorand@gmail.com>
2016-06-26 14:17:42 +0200

    test 4

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+/*
+ Third party
+ */
+
+//= ../../node_modules/a11y-toggle/a11y-toggle.js
+//= ../../bower_components/respond/dest/respond.min.js
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/waypoints/lib/jquery.waypoints.min.js
+
+
+
+/*
+Custom
+ */
+
+//= partials/helper.js
+//= partials/google-map.js
+//= partials/animate-css.js
+
+
+// Preloader 
+
